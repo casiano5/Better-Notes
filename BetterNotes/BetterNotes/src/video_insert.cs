c9a60b4e0c3e031a21c4713c7268afd9fab36753
Shane Casiano <shane.casiano@gmail.com>
2021-02-03 19:31:07 -0500

    Updated .gitignore, video_insert
    added more imports for video_insert

--- BetterNotes/BetterNotes/src/video_insert.cs
+++ BetterNotes/BetterNotes/src/video_insert.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace BetterNotes{
     class video_insert{
