8819c2e35be8409f2fd6020ca00f40c050bd78ae
Shane Casiano <shane.casiano@gmail.com>
2021-02-03 22:15:49 -0500

    Updated video_insert.cs
    Added download_video function

--- BetterNotes/BetterNotes/src/video_insert.cs
+++ BetterNotes/BetterNotes/src/video_insert.cs
@@ -3,19 +3,24 @@ using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 using YoutubeExtractor;
 
 namespace BetterNotes{
     class video_insert{
-        public static void download_video(){
-            
+        public static void download_video(string link){
+            IEnumerable<VideoInfo> video_info = DownloadUrlResolver.GetDownloadUrls(link);
+            VideoInfo video = video_info.First(info => info.VideoType == VideoType.Mp4 && info.Resolution == 360); //This will download at 360p, thats really low but YT may not have a 720p available so that could break it if it was higher, maybe fix?
+            if (video.RequiresDecryption) {
+                DownloadUrlResolver.DecryptDownloadUrl(video);
+            }
+            var videoDownloader = new VideoDownloader(video, Path.Combine("C:\Users\Public\Downloads", video.Title + video.VideoExtension)); //Temporary download location, needs to be program install path whenever that happens, or needs to be linked with a prop file.
+            videoDownloader.DownloadProgressChanged += (sender, args) => Console.WriteLine(args.ProgressPercentage); //writing percentage complete to console, will eventually map to GUI element
+            videoDownloader.Execute();
         }
-        public static string get_link(){
+        public static void get_link(){
             string video_link;
-
-            return video_link;
+            //Search for youtube video, send link to above
+            download_video(video_link);
         }
     }
 }
