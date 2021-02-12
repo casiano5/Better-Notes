d84c71aa7263a204fdf04b2d0638f6ae914e9595
Shane Casiano <shane.casiano@gmail.com>
2021-02-12 18:30:09 -0500

    Added support for email and phone sending
    NotesReminder and EmailSMTP doc to send email, or convert phone to email and send email. Still need to add timer system (part of integration step)
    Where will reminder be saved?
    How will program know when to send reminder?
    Must program be running to send reminder?
    Where will address to send reminder to be saved?

---
+++ BetterNotes/BetterNotes/dependancies/EmailSMTPService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace BetterNotes {
+    public class EmailSMTPService {
+        public static int sendMail(MailMessage message) {
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com") {
+                Port = 587,
+                Credentials = new NetworkCredential("betternotesproject@gmail.com", ""), //password to be filled in for gmail smtp account. ONLY FOR TESTING PURPOSES.
+                EnableSsl = true
+            };
+            smtpClient.Send(message);
+            return 0;
+        }
+    }
+}
--- BetterNotes/BetterNotes/src/NotesReminder.cs
+++ BetterNotes/BetterNotes/src/NotesReminder.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Net.Mail;
 using Windows.UI.Notifications;
 using Microsoft.Toolkit.Uwp.Notifications;
 using Windows.Data.Xml.Dom;
 
 namespace BetterNotes {
     public class NotesReminder {
+        //Windows Toast Notification
         void sendWindowsToastNotification(string toastTitleContent) {
 
             ToastContent toastContent = new ToastContent() {
@@ -33,6 +37,79 @@ namespace BetterNotes {
             /*Send Notification*/
             DesktopNotificationManagerCompat.CreateToastNotifier().Show(toast);
 
+        }
+
+        //Email notification
+        void sendPhoneEmailNotification(string contactInformation, string reminderTitle, string reminderBody) {
+            if (isValidEmail(contactInformation)) {
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress("betternotes@casiano.dev"),
+                    Subject = "Your Reminder for: " + reminderTitle,
+                    Body = "<h1>Reminder: " + reminderTitle + "</h1>"
+                            + "<p>" + reminderBody + "</p>",
+                    IsBodyHtml = true,
+                };
+                mailMessage.To.Add(contactInformation);
+                EmailSMTPService.sendMail(mailMessage);
+            }
+
+            if (isValidPhoneNumber(contactInformation)) {
+                string email = convertPhoneToEmail(contactInformation);
+                var mailMessage = new MailMessage
+                {
+                    From = new MailAddress("betternotes@casiano.dev"),
+                    Subject = "Your Reminder for: " + reminderTitle,
+                    Body = "<h1>Reminder: " + reminderTitle + "</h1>"
+                           + "<p>" + reminderBody + "</p>",
+                    IsBodyHtml = true,
+                };
+                mailMessage.To.Add(email);
+                EmailSMTPService.sendMail(mailMessage);
+            }
+        }
+        //Check for email func
+        public static bool isValidEmail(string email) {
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+            try {
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper, RegexOptions.None, TimeSpan.FromMilliseconds(200));
+                string DomainMapper(Match match) {
+                    var idn = new IdnMapping();
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException e) {
+                return false;
+            }
+            catch (ArgumentException e) {
+                return false;
+            }
+            try {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException) {
+                return false;
+            }
+        }
+
+        //Check for phone func
+        public static bool isValidPhoneNumber(string number) {
+            int hold = 0;
+            return int.TryParse(number.Substring(4), out hold);
+
+        }
+        //append email for carrier based on original string with character (eg: VZW8314786726 becomes 8314786726@vtext.com)
+        public static string convertPhoneToEmail(string phoneNumber) {
+            string rtrn = "";
+            if (phoneNumber.Contains("VZW")) rtrn = phoneNumber.Substring(4) + "@vtext.com";
+            if (phoneNumber.Contains("ATT")) rtrn = phoneNumber.Substring(4) + "@txt.att.net";
+            if (phoneNumber.Contains("TMO")) rtrn = phoneNumber.Substring(4) + "@tmomail.net";
+            if (isValidEmail(rtrn)) return rtrn;
+            return "betternotesfailedphonetoemail@casiano.dev";
         }
     }
 }
