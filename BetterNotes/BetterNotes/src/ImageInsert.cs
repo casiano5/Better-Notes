d314e26f87663a8206bd64ea470c04dca74275e2
Shane Casiano <shane.casiano@gmail.com>
2021-02-15 23:47:36 -0500

    Updated ImageInsert and VideoInsert
    VideoInsert
       - Added function for search term completion
       - Added framework for get videos from url
    
    ImageInsert
       - Added function to get images from search term
       - Added function to get images from url, returns list of image urls (gstatic)

--- BetterNotes/BetterNotes/src/ImageInsert.cs
+++ BetterNotes/BetterNotes/src/ImageInsert.cs
@@ -1,11 +1,24 @@
 using System;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace BetterNotes{
-    class ImageInsert{
-
+    public class ImageInsert{
+        //TODO: Function to download images and save them in the correct path, return the path of the saved image (path given based on notes requesting insert)
+        public static List<string> getImagesFromSearchTerm(string searchTerm) {
+            const string imageSearchUrl = "https://www.google.com/search?site=&tbm=isch&q=";
+            searchTerm = WebUtility.UrlEncode(searchTerm);
+            return getImagesFromUrl(imageSearchUrl + searchTerm);
+        }
+        public static List<string> getImagesFromUrl(string url) {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument imageDocument = web.Load(url);
+            List<string> links = imageDocument.DocumentNode.SelectNodes("//img").Select(b => b.Attributes["src"].Value).ToList();
+            List<string> filteredLinks = new List<string>();
+            foreach (string link in links) if (link.Contains("https://")) filteredLinks.Add(link);
+            return filteredLinks;
+        }
     }
 }
--- BetterNotes/BetterNotes/src/VideoInsert.cs
+++ BetterNotes/BetterNotes/src/VideoInsert.cs
@@ -1,12 +1,19 @@
 using System;
-using System.Net;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
+using System.Net;
+using HtmlAgilityPack;
 
 namespace BetterNotes{
     class VideoInsert {
-
+        //TODO: Function to return embed for videos to insert into notes. (OR maybe url????????)
+        public static List<string> getVideosFromSearchTerm(string searchTerm) {
+            const string videoSearchUrl = "https://www.youtube.com/results?search_query=";
+            searchTerm = WebUtility.UrlEncode(searchTerm);
+            return getVideosFromUrl(videoSearchUrl + searchTerm);
+        }
+        public static List<string> getVideosFromUrl(string url) {
+            //TODO: Implement
+            return new List<string>();
+        }
     }
 }
