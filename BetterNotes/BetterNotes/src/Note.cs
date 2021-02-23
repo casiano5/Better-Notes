6ac1c56cb4a6f7371f687631ff93895f19ce0ec1
Shane Casiano <shane.casiano@gmail.com>
2021-02-22 23:48:04 -0500

    Added structure for Note and Reminder Classes
    Note class added with a ton of TODOs
    - Reminder class is a class of type Note, it just has reminder features,
    - See comments on Note.cs for more info

---
+++ BetterNotes/BetterNotes/src/Note.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+
+//TODO CREATE STATIC FINAL ENV VAR FOR WORKING DIRECTORY OF BETTERNOTES AND STORE IT SOMEWHERE
+
+
+namespace BetterNotes {
+    public class Note {
+        //private vars and get/sets
+        public string FilePath { get; set; } //set only in case you want to move the and change property while open (renaming notes, temp directories, etc)
+        public string Name { get; set; } 
+        public string CreateUser { get; set; } 
+        public DateTime CreatedDateTime { get; } //RONLY value, should not change after first creation
+        public DateTime LastModifiedDateTime { get; set; } 
+        private string note; //PLACEHOLDER VALUE 
+
+        //make a note object (new file)
+        public Note(string workDir, string name, string createUser) { 
+            this.FilePath = workDir + "\\" + name;
+            this.Name = name;
+            this.CreateUser = createUser;
+            this.CreatedDateTime = DateTime.Now;
+            this.LastModifiedDateTime = DateTime.Now;
+        }
+
+        //make a note object (existing file)
+        public Note(string currentNoteDirectory) {
+            //TODO make constructor for note based on existing note file
+            //constructor will expect a directory 
+            //modify date time modified, read other info from possible metadata file
+            //TODO design metadata file
+        }
+
+        //TODO make a close note function that calls a object save/delete function based on user input
+        //TODO make a save function that calls a static save function
+        //TODO make a delete function that calls a static function to delete a note file
+        //TODO make a destructor for note object after delete or save
+        //TODO make a function that saves note metadata to BetterNotes metadata files foe easy access (recent notes)
+        
+    }
+
+}
---
+++ BetterNotes/BetterNotes/src/Reminder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterNotes {
+    public class Reminder : Note {
+        public DateTime SendReminderDateTime { get; set; }
+
+        public Reminder(string workDir, string name, string createUser) : base(workDir + "\\" + name, name, createUser) {
+            //just creates a note object, theres only one difference between a note and a reminder, one of them can alert and the other can not
+        }
+
+        //make a note object (existing file)
+        public Reminder(string currentNoteDirectory) : base(currentNoteDirectory){
+            //TODO: Read field "whenToRemind" in metadata and set sendReminderDateTime to this value
+        }
+        //TODO: Make destructor for Reminder class
+    }
+}
