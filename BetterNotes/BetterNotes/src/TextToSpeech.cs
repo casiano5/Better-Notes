c48edff221bb40b38c83b9c5bd319e81f91dab00
Jakelfrench21 <77357695+Jakelfrench21@users.noreply.github.com>
2021-03-04 13:29:15 -0500

    Reformatted code TTS
    made static functions

--- BetterNotes/BetterNotes/src/TextToSpeech.cs
+++ BetterNotes/BetterNotes/src/TextToSpeech.cs
@@ -1,107 +1,17 @@
-using System;
-using System.ComponentModel;
-using System.IO;
 using System.Speech.Synthesis;
-using System.Windows.Forms;
-namespace TextToSpeech
+namespace BetterNotes
 {
-    public partial class Form1 : Form
-    {
-        SpeechSynthesizer reader; 
-        public Form1()
-        {
-            InitializeComponent();
+    public class TextToSpeech{
+        static void getSpeech(string input, SpeechSynthesizer reader){
+           reader = new SpeechSynthesizer();
+           reader.SetOutputToDefaultAudioDevice();
+           reader.Speak(input);
+        }//getspeech
 
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            reader = new SpeechSynthesizer(); 
-            button2.Enabled = false;
-            button3.Enabled = false;
-            button4.Enabled = false;
-            textBox1.ScrollBars = ScrollBars.Both;
-        }
-
-        //SPEAK TEXT
-        private void button1_Click(object sender, EventArgs e)
-        {
-            reader.Dispose();
-            if (textBox1.Text != "")    //if text area is not empty
-            {
-
-                reader = new SpeechSynthesizer();
-                reader.SpeakAsync(textBox1.Text);
-                label2.Text = "SPEAKING";
-                button2.Enabled = true;
-                button4.Enabled = true;
-                reader.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs> (reader_SpeakCompleted);
-            }
-            else
-            {
-                object err = MessageBox.Show("Please enter some notes", "Message", MessageBoxButtons.OK);
-            }
-        }
-
-        //event handler
-        void reader_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
-        {
-            label2.Text = "IDLE";
-        }
-
-        //PAUSE
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (reader != null)
-            {
-                if (reader.State == SynthesizerState.Speaking)
-                {
-                    reader.Pause();
-                    label2.Text = "PAUSED";
-                    button3.Enabled = true;
-
-                }
-            }
-        }
-
-        //RESUME
-        private void button3_Click(object sender, EventArgs e)
-        {
-            if (reader != null)
-            {
-                if (reader.State == SynthesizerState.Paused)
-                {
-                    reader.Resume();
-                    label2.Text = "SPEAKING";
-                }
-                button3.Enabled = false;
-            }
-        }
-
-        //STOP
-        private void button4_Click(object sender, EventArgs e)
-        {
-            if (reader != null)
-            {
-                reader.Dispose();
-                label2.Text = "IDLE";
-                button2.Enabled = false;
-                button3.Enabled = false;
-                button4.Enabled = false;
-            }
-        }
-
-        //load data from text file button
-        private void button5_Click(object sender, EventArgs e)
-        {
-            openFileDialog1.ShowDialog();
-        }
-
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-            //copy data from text file and load it to textbox
-            textBox1.Text = File.ReadAllText(openFileDialog1.FileName.ToString());
-
-        }
-
-    }
-}
+         static void putSpeechInFile(string input, SpeechSynthesizer reader){
+            reader = new SpeechSynthesizer();
+            reader.SetOutputToWaveFile(@"C:\textToSpeech.wav");
+            reader.Speak(input);
+        }//file
+    }//class 
+}//namespace
