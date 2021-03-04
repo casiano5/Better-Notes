97f6d213ce3cde30acd4cf8d28761d54e183df1d
Jakelfrench21 <77357695+Jakelfrench21@users.noreply.github.com>
2021-03-04 18:58:32 -0500

    fix forms
    fixed project error

--- BetterNotes/BetterNotes/src/TextToSpeech.cs
+++ BetterNotes/BetterNotes/src/TextToSpeech.cs
@@ -1,19 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Speech.Synthesis;
+
 namespace BetterNotes
 {
-    public class TextToSpeech{
-        static void GetSpeech(string input){
-           SpeechSynthesizer reader = new SpeechSynthesizer();
-           reader.SetOutputToDefaultAudioDevice();
-           reader.Speak(input);
-           reader.Dispose();
-        }//getspeech
+    class TextToSpeech
+    {
+            static void GetSpeech(string input)
+            {
+                SpeechSynthesizer reader = new SpeechSynthesizer();
+                reader.SetOutputToDefaultAudioDevice();
+                reader.Speak(input);
+                reader.Dispose();
+            }//getspeech
 
-         static void PutSpeechInFile(string input, string path){
-            SpeechSynthesizer reader = new SpeechSynthesizer();
-            reader.SetOutputToWaveFile(path);
-            reader.Speak(input);
-            reader.Dispose();
-        }//file
-    }//class 
-}//namespace
+            static void PutSpeechInFile(string input, string path)
+            {
+                SpeechSynthesizer reader = new SpeechSynthesizer();
+                reader.SetOutputToWaveFile(path);
+                reader.Speak(input);
+                reader.Dispose();
+            }//file
+     }//class 
+    
+}
