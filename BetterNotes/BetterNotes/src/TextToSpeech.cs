98f71d94130574b87a1a69e99b72b3cd6ab76391
Jakelfrench21 <77357695+Jakelfrench21@users.noreply.github.com>
2021-03-04 13:33:05 -0500

    small code update

--- BetterNotes/BetterNotes/src/TextToSpeech.cs
+++ BetterNotes/BetterNotes/src/TextToSpeech.cs
@@ -5,13 +5,13 @@ namespace BetterNotes
         static void getSpeech(string input, SpeechSynthesizer reader){
            reader = new SpeechSynthesizer();
            reader.SetOutputToDefaultAudioDevice();
-           reader.Speak(input);
+           reader.Speak(input.ToString());
         }//getspeech
 
          static void putSpeechInFile(string input, SpeechSynthesizer reader){
             reader = new SpeechSynthesizer();
             reader.SetOutputToWaveFile(@"C:\textToSpeech.wav");
-            reader.Speak(input);
+            reader.Speak(input.ToString());
         }//file
     }//class 
 }//namespace
