a5a68d76eee4b134dbb3c02acc280986e379a8a9
Shane Casiano <shane.casiano@gmail.com>
2021-03-16 19:32:35 -0400

    Update BetterNotesMainView.xaml.cs

--- BetterNotes/BetterNotesGUI/BetterNotesMainView.xaml.cs
+++ BetterNotes/BetterNotesGUI/BetterNotesMainView.xaml.cs
@@ -1,21 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Forms;
 using BetterNotes;
 using System.IO;
-using System.ComponentModel;
+using Microsoft.Win32;
 
 //TODO: Connect the buttons
 //TODO: Add an event listener, on change of richtextbox, set bool to false and prompt user if they want to save on close event.
