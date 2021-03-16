edf8b9109d3733ab8f5eb8d019553b4205e3ce2f
Shane Casiano <shane.casiano@gmail.com>
2021-03-16 16:46:47 -0400

    Small changes

--- BetterNotes/BetterNotes/src/UserHandler.cs
+++ BetterNotes/BetterNotes/src/UserHandler.cs
@@ -7,16 +7,17 @@ using System.Threading.Tasks;
 
 namespace BetterNotes {
     public static class UserHandler {
-        public static List<Users> AddAllUsersInMetadata() {
-            List<Users> userList = new List<Users>();
+        public static List<Users> userList;
+        public static void AddAllUsersInMetadata() {
+            List<Users> userListTemp = new List<Users>();
             using (var reader = new StreamReader(GlobalVars.BnotUsersCsv)) {
                 while (!reader.EndOfStream) {
                     string line = reader.ReadLine();
                     string[] temp = line.Split(',');
-                    userList.Add(new Users(temp[0], temp[1], temp[2]));
+                    userListTemp.Add(new Users(temp[0], temp[1], temp[2]));
                 }
             }
-            return userList;
+            userList = userListTemp;
         }
     }
 }
