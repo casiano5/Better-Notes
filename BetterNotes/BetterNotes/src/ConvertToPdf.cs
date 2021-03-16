b1d8cff7e2808fb6272ef62e96c1b0f2aba32ec2
Shane Casiano <shane.casiano@gmail.com>
2021-03-16 01:51:45 -0400

    Oops

--- BetterNotes/BetterNotes/src/ConvertToPdf.cs
+++ BetterNotes/BetterNotes/src/ConvertToPdf.cs
@@ -5,7 +5,7 @@ using System.Windows.Documents;
 
 namespace BetterNotes {
     class ConvertToPdf {
-        public static void convert(RichTextBox noteContent) {
+        public static void Convert(RichTextBox noteContent) {
             PrintDialog pd = new PrintDialog();
             pd.PrintQueue = new PrintQueue(new PrintServer(), "Microsoft Print to PDF");
             pd.PrintDocument((((IDocumentPaginatorSource)noteContent.Document).DocumentPaginator), "Save to PDF");
